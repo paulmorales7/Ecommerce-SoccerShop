710f20ad7768366a8e1a653a918ab6bd26756f71
Paul Morales <paulginga@hotmail.com>
2021-02-18 19:42:32 -0600

    adding scroll event in main.js

--- main.js
+++ main.js
@@ -1,10 +1,31 @@
 
 function toggleMenu() {
     var menuBox = document.getElementById('nav-menu');    
-    if(menuBox.style.display == "block") { // if is menuBox displayed, hide it
+    if(menuBox.style.display == "block") { 
       menuBox.style.display = "none";
     }
-    else { // if is menuBox hidden, display it
+    else {
       menuBox.style.display = "block";
     }
+  }
+
+  const sections = document.querySelectorAll('section[id')
+
+  window.addEventListener('scroll', scrollActive)
+
+  function scrollActive(){
+      const scrollY = window.pageYOffset
+
+      sections.forEach(current =>{
+          const sectionHeight = current.offsetHeight
+          const sectionTop = current.offsetTop - 50
+          sectionId = current.getAttribute('id')
+
+          if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+              document.querySelector('nav__menu a[href*='+ sectionId +']').classList.add('active')
+              
+          }else{
+            document.querySelector('nav__menu a[href*='+ sectionId +']').classList.remove('active')
+          }
+      })
   }
