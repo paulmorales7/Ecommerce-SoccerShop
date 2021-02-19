f067a5013c9612798b3c55c004746897b256f989
Paul Morales <paulginga@hotmail.com>
2021-02-18 20:01:08 -0600

    adding onScroll to header section, then starting on home section

--- main.js
+++ main.js
@@ -30,4 +30,8 @@ function toggleMenu() {
       })
   }
 
-  
+  window.onscroll = ()=>{
+      const nav = document.getElementById('header')
+        if(this.scrollY >= 200) nav.classList.add('scroll-header'); 
+    else nav.classList.remove('scroll-header')
+    }
